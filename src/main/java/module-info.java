bdb24a80bdd6bab73dddcabfccbc8361155c9727
JMisley <johnny.misley@gmail.com>
2022-03-09 16:58:09 -0500

    Added more styling

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.risjavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires MaterialFX;
 
     opens com.risjavafx to javafx.fxml;
     exports com.risjavafx;
