0d65cc5594b08cc4fdb4a8c265355f6937c302b5
Cal C <59923721+MendokusaiCode@users.noreply.github.com>
2022-03-09 17:02:37 -0500

    need to figure out flag colum error

---
+++ src/main/java/com/risjavafx/UserInfoData.java
@@ -0,0 +1,56 @@
+package com.risjavafx;
+
+import javafx.beans.property.SimpleStringProperty;
+
+@SuppressWarnings("unused")
+public class UserInfoData {
+    SimpleStringProperty userIdData;
+    SimpleStringProperty usernameData;
+    SimpleStringProperty displayNameData;
+    SimpleStringProperty emailAddressData;
+    SimpleStringProperty systemRoleData;
+
+    public UserInfoData(String userId, String username, String displayName, String emailAdr, String systemRole) {
+        this.userIdData = new SimpleStringProperty(userId);
+        this.usernameData = new SimpleStringProperty(username);
+        this.displayNameData = new SimpleStringProperty(displayName);
+        this.emailAddressData = new SimpleStringProperty(emailAdr);
+        this.systemRoleData = new SimpleStringProperty(systemRole);
+    }
+
+    public String getUserIdData() {return userIdData.get();}
+
+    public void setUserIdData(String USERID) {this.userIdData.set(USERID);}
+
+    public String getUsernameData() {
+        return usernameData.get();
+    }
+
+    public void setUsernameData(String usernameData) {
+        this.usernameData.set(usernameData);
+    }
+
+    public String getDisplayNameData() {
+        return displayNameData.get();
+    }
+
+    public void setDisplayNameData(String displayNameData) {
+        this.displayNameData.set(displayNameData);
+    }
+
+    public String getEmailAddressData() {
+        return emailAddressData.get();
+    }
+
+    public void setEmailAddressData(String emailAddressData) {
+        this.emailAddressData.set(emailAddressData);
+    }
+
+    public String getSystemRoleData() {
+        return systemRoleData.get();
+    }
+
+    public void setSystemRoleData(String systemRoleData) {
+        this.systemRoleData.set(systemRoleData);
+    }
+}
