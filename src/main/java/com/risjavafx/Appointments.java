f39ae6cbbebb9552fead32fe2f4d0aa086dd8ba7
Cal C <59923721+MendokusaiCode@users.noreply.github.com>
2022-03-09 17:02:18 -0500

    need to figure out flag colum error

--- src/main/java/com/risjavafx/Appointments.java
+++ src/main/java/com/risjavafx/Appointments.java
@@ -1,27 +1,118 @@
 package com.risjavafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
+import javax.xml.transform.Result;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Appointments implements Initializable {
 
     public HBox topContent;
+    public StackPane centerContent;
+    public TableColumn<AppointmentData, String>
+            patient = new TableColumn<>("Patient"),
+            modality = new TableColumn<>("Modality"),
+            price = new TableColumn<>("Price"),
+            dateTime = new TableColumn<>("Date"),
+            radiologist = new TableColumn<>("Radiologist"),
+            technician = new TableColumn<>("Technician"),
+            closedFlag = new TableColumn<>("Closed");
+
+    public ArrayList<TableColumn<AppointmentData, String>> tableColumnsList = new ArrayList<>(){{
+         add(patient); add(modality); add(price); add(dateTime); add(radiologist); add(technician);
+         add(closedFlag);
+    }};
+
 
     // Load NavigationBar component into home-page.fxml
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        createNavBar();
+
+        try {
+            createTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createNavBar() {
         try {
             URL navigationBarComponent = getClass().getResource("fxml components/NavigationBar.fxml");
             topContent.getChildren().setAll((Node) FXMLLoader.load(Objects.requireNonNull(navigationBarComponent)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void createTable() throws SQLException {
+        queryData();
+        setCellFactoryValues();
+        InfoTable<AppointmentData> infoTable = new InfoTable<>(){{
+            setColumns(tableColumnsList);
+            addColumnsToTable();
+            tableView.setMaxWidth(1150);
+            tableView.setMaxHeight(650);
+            tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            tableView.setStyle("styles.css");
+        }};
+        centerContent.getChildren().add(infoTable.tableView);
+        infoTable.tableView.setItems(queryData());
+    }
+
+    public ObservableList<AppointmentData> queryData() throws SQLException {ObservableList<AppointmentData> observableList = FXCollections.observableArrayList();
+        Driver driver = new Driver();
+        ResultSet resultSet = driver.connection.createStatement().executeQuery("""
+                SELECT *
+                FROM appointments, modalities, patients, users
+                WHERE modality_id = modality AND user_id = radiologist
+              
+                """);
+
+
+        while (resultSet.next()) {
+            String name = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+            String checkIn;
+            if (resultSet.getByte("closed") == 1) {
+                checkIn = "concluded";
+            } else {
+                checkIn = "In Progress";
+            }
+            observableList.add(new AppointmentData(
+                    name,
+                    resultSet.getString("name"),
+                    resultSet.getString("price"),
+                    resultSet.getDate("date_time").toString(),
+                    resultSet.getString("radiologist"),
+                    resultSet.getString("technician"),
+                    checkIn
+            ));
+        }
+        return observableList;
+    }
+
+    public void setCellFactoryValues() {
+        patient.setCellValueFactory(new PropertyValueFactory<>("Patient"));
+        modality.setCellValueFactory(new PropertyValueFactory<>("Modality"));
+        price.setCellValueFactory(new PropertyValueFactory<>("Price"));
+        dateTime.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        radiologist.setCellValueFactory(new PropertyValueFactory<>("Radiologist"));
+        technician.setCellValueFactory(new PropertyValueFactory<>("Technician"));
+        closedFlag.setCellValueFactory(new PropertyValueFactory<>("closed"));
     }
 }
