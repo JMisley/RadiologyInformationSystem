f06d3839dd3adbf1905e26a760bf17942a4ffe70
JMisley <johnny.misley@gmail.com>
2022-03-09 19:10:02 -0500

    Commit before pulling main branch

--- src/main/java/com/risjavafx/InfoTable.java
+++ src/main/java/com/risjavafx/InfoTable.java
@@ -5,10 +5,10 @@ import javafx.scene.control.TableView;
 
 import java.util.ArrayList;
 
-public class InfoTable<T, K> {
+public class InfoTable<T> {
 
     public TableView<T> tableView;
-    public ArrayList<TableColumn<T, K>> columnsArrayList = new ArrayList<>();
+    public ArrayList<TableColumn<T, String>> columnsArrayList = new ArrayList<>();
 
 
     public InfoTable() {
@@ -16,12 +16,12 @@ public class InfoTable<T, K> {
         tableView.setColumnResizePolicy(resizeFeatures -> false);
     }
 
-    public void setColumns(ArrayList<TableColumn<T, K>> columnsArrayList) {
+    public void setColumns(ArrayList<TableColumn<T, String>> columnsArrayList) {
         this.columnsArrayList = columnsArrayList;
     }
 
     public void addColumnsToTable() {
-        for (TableColumn<T, K> tableColumn : columnsArrayList) {
+        for (TableColumn<T, String> tableColumn : columnsArrayList) {
             tableView.getColumns().add(tableColumn);
             tableColumn.setReorderable(false);
             tableColumn.setSortable(true);
@@ -29,7 +29,7 @@ public class InfoTable<T, K> {
     }
 
     // Allows you to specify what column width percent you want for each column
-    public void setCustomColumnWidth(TableColumn<T, K> column, double width) {
+    public void setCustomColumnWidth(TableColumn<T, String> column, double width) {
         column.prefWidthProperty().bind(tableView.widthProperty().multiply(width));
     }
 }
